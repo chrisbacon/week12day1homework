c18c1f123f93616bef118de6f32a7fa60fba233a
chrisbacon <bacon.chris@gmail.com>
2017-01-30 18:15:41 +0000

    linked up controllers to server.js

--- controllers/films.js
+++ controllers/films.js
@@ -2,3 +2,9 @@
 var films = require('../client/src/models/films')();
 var Film = require('../client/src/models/film');
 var Review = require('../client/src/models/review');
+
+var express = require('express');
+
+var filmRouter = express.Router();
+
+module.exports = filmRouter;
